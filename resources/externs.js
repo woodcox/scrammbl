021ec0cf2988b5a2b91ca844ee390d8fc0edea0e
Rustie <64870518+woodcox@users.noreply.github.com>
2024-09-12 22:31:20 +0100

    Update externs.js

--- resources/externs.js
+++ resources/externs.js
@@ -5,21 +5,22 @@
  */
 
 /**
- * @type {{level1: Array<{index: string, correctPositions: Object<string, string>}>, 
-*         level2: Array<{index: string, correctPositions: Object<string, string>}>, 
-*         level3: Array<{index: string, correctPositions: Object<string, string}>},
-*         level4: Array<{index: string, correctPositions: Object<string, string}>},
-*         level5: Array<{index: string, correctPositions: Object<string, string}>},
-*         level6: Array<{index: string, correctPositions: Object<string, string}>},
-*         level7: Array<{index: string, correctPositions: Object<string, string}>},
-*         level8: Array<{index: string, correctPositions: Object<string, string}>},
-*         level9: Array<{index: string, correctPositions: Object<string, string}>},
-*         level10: Array<{index: string, correctPositions: Object<string, string}>},
-*         level11: Array<{index: string, correctPositions: Object<string, string}>},
-*         level12: Array<{index: string, correctPositions: Object<string, string}>},
-*         level13: Array<{index: string, correctPositions: Object<string, string}>}
-*        }}
-*/
+ * @type {{
+ *   level1: Array<{index: string, correctPositions: {[key: string]: string}}>,
+ *   level2: Array<{index: string, correctPositions: {[key: string]: string}}>,
+ *   level3: Array<{index: string, correctPositions: {[key: string]: string}}>,
+ *   level4: Array<{index: string, correctPositions: {[key: string]: string}}>,
+ *   level5: Array<{index: string, correctPositions: {[key: string]: string}}>,
+ *   level6: Array<{index: string, correctPositions: {[key: string]: string}}>,
+ *   level7: Array<{index: string, correctPositions: {[key: string]: string}}>,
+ *   level8: Array<{index: string, correctPositions: {[key: string]: string}}>,
+ *   level9: Array<{index: string, correctPositions: {[key: string]: string}}>,
+ *   level10: Array<{index: string, correctPositions: {[key: string]: string}}>,
+ *   level11: Array<{index: string, correctPositions: {[key: string]: string}}>,
+ *   level12: Array<{index: string, correctPositions: {[key: string]: string}}>,
+ *   level13: Array<{index: string, correctPositions: {[key: string]: string}}>,
+ * }}
+ */
 gameDataArray
 
 /** @type {{a1: string, a2: string, a3: string, a4: string, b4: string, c4: string, d4: string, e4: string, b1: string, c1: string, d1: string, e1: string, e2: string, e3: string}} */

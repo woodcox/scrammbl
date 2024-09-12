2c0543d065dc66db8266e5f7679ec8cf6b2afa41
Rustie <64870518+woodcox@users.noreply.github.com>
2024-09-12 07:26:53 +0100

    Added correct positions to extern.js

--- resources/externs.js
+++ resources/externs.js
@@ -22,6 +22,9 @@
 */
 gameDataArray
 
+/** @type {{a1: string, a2: string, a3: string, a4: string, b4: string, c4: string, d4: string, e4: string, b1: string, c1: string, d1: string, e1: string, e2: string, e3: string}} */
+correctPositions
+
 /**
  * @constructor
  * @extends {HTMLElement}

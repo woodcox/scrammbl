15d16867c09bde83c538c103d39b84d833e53604
Rustie <64870518+woodcox@users.noreply.github.com>
2024-09-12 23:26:37 +0100

    Update externs.js

--- resources/externs.js
+++ resources/externs.js
@@ -21,10 +21,24 @@
  *   level13: Array<{index: string, correctPositions: {[key: string]: string}}>,
  * }}
  */
-var gameDataArray;
+var gameDataArray = {
+  "level1": [{ "index": "", "correctPositions": { "a2": "", "a3": "" }}],
+  "level2": [{ "index": "", "correctPositions": { "a1": "", "a2": "", "a3": "" }}],
+  "level3": [{ "index": "", "correctPositions": { "a1": "", "a2": "", "a3": "", "a4": "" }}],
+  "level4": [{ "index": "", "correctPositions": { "a1": "", "a2": "", "a3": "", "a4": "", "b4": "" }}],
+  "level5": [{ "index": "", "correctPositions": { "a1": "", "a2": "", "a3": "", "a4": "", "b4": "", "c4": "" }}],
+  "level6": [{ "index": "", "correctPositions": { "a1": "", "a2": "", "a3": "", "a4": "", "b4": "", "c4": "", "d4": "" }}],
+  "level7": [{ "index": "", "correctPositions": { "a1": "", "a2": "", "a3": "", "a4": "", "b4": "", "c4": "", "d4": "", "e4": "" }}],
+  "level8": [{ "index": "", "correctPositions": { "a1": "", "a2": "", "a3": "", "a4": "", "b4": "", "c4": "", "d4": "", "e4": "", "b1": "" }}],
+  "level9": [{ "index": "", "correctPositions": { "a1": "", "a2": "", "a3": "", "a4": "", "b4": "", "c4": "", "d4": "", "e4": "", "b1": "", "c1": "" }}],
+  "level10": [{ "index": "", "correctPositions": { "a1": "", "a2": "", "a3": "", "a4": "", "b4": "", "c4": "", "d4": "", "e4": "", "b1": "", "c1": "", "d1": "" }}],
+  "level11": [{ "index": "", "correctPositions": { "a1": "", "a2": "", "a3": "", "a4": "", "b4": "", "c4": "", "d4": "", "e4": "", "b1": "", "c1": "", "d1": "", "e1": "" }}],
+  "level12": [{ "index": "", "correctPositions": { "a1": "", "a2": "", "a3": "", "a4": "", "b4": "", "c4": "", "d4": "", "e4": "", "b1": "", "c1": "", "d1": "", "e1": "", "e2": "" }}],
+  "level13": [{ "index": "", "correctPositions": { "a1": "", "a2": "", "a3": "", "a4": "", "b4": "", "c4": "", "d4": "", "e4": "", "b1": "", "c1": "", "d1": "", "e1": "", "e2": "", "e3": "" }}]
+};
 
 /** @type {{a1: string, a2: string, a3: string, a4: string, b4: string, c4: string, d4: string, e4: string, b1: string, c1: string, d1: string, e1: string, e2: string, e3: string}} */
-var correctPositions;
+var correctPositions = {{"a1": "", "a2": "", "a3": "", "a4": "", "b4": "", "c4": "", "d4": "", "e4": "", "b1": "", "c1": "", "d1": "", "e1": "", "e2": "", "e3": ""}};
 
 /**
  * @constructor
